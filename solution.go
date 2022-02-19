2bbadf332af741bbffae02e89292b244775170a4
Uriy3 <boginskiy291082@gmail.com>
2022-02-20 00:36:16 +0300

    My try

--- solution.go
+++ solution.go
@@ -1,6 +1,6 @@
 package square
 
-import ( 
+import (
 "math"
 )
 
@@ -9,15 +9,38 @@ type myType int
 
 func CalcSquare(sideLen float64, sidesNum myType) (res float64) {
 
-	const SidesTriangle myType = 3
-	const SidesSquare myType = 4
-	const SidesCircle myType = 0
-	if SidesTriangle == sidesNum {
-		res = (math.Pow(sideLen, 2.0) * math.Sqrt(3.0))/4
-	} else if SidesSquare == sidesNum {
-		res = math.Pow(sideLen, 2.0)
-	} else if SidesCircle == sidesNum {
-		res = math.Pi * math.Pow(sideLen, 2.0)
-	}
-	return
+        const SidesTriangle myType = 3
+        const SidesSquare myType = 4
+        const SidesCircle myType = 0
+        if SidesTriangle == sidesNum {
+                res = (math.Pow(sideLen, 2.0) * math.Sqrt(3.0))/4
+        } else if SidesSquare == sidesNum {
+                res = math.Pow(sideLen, 2.0)
+        } else if SidesCircle == sidesNum {
+                res = math.Pi * math.Pow(sideLen, 2.0)
+        }
+        return
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
