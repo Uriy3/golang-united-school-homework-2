f4c6f124360c14499f983c78aac5828281eaccac
Uriy3 <99492882+Uriy3@users.noreply.github.com>
2022-02-20 01:46:28 +0300

    Update solution.go

--- solution.go
+++ solution.go
@@ -1,32 +1,28 @@
-package square
-	
-	import "math"
-	
-	type CustomType = int
-	
-	const Pi = 3.14159
-	const SidesCircle = 0
-	const SidesTriangle = 3
-	const SidesSquare = 4
-	
-	// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
-	
-	// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-	// it's like:
-	// CalcSquare(10.0, SidesTriangle)
-	// CalcSquare(10.0, SidesSquare)
-	// CalcSquare(10.0, SidesCircle)
-	//sideLen float64, sidesNum CustomType
-	
-	func CalcSquare(sideLen float64, sidesNum CustomType) float64 {
-		switch sidesNum {
-		case SidesSquare:
-			return sideLen * sideLen
-		case SidesTriangle:
-			return (math.Sqrt(3) / 4) * sideLen * sideLen
-		case SidesCircle:
-			return Pi * sideLen * sideLen
-		default:
-			return 0
-		}
-	}
+ppackage square
+
+import "math"
+
+// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
+
+// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
+// it's like:
+// CalcSquare(10.0, SidesTriangle)
+// CalcSquare(10.0, SidesSquare)
+// CalcSquare(10.0, SidesCircle)
+
+type myType int
+
+func CalcSquare(sideLen float64, sidesNum myType) (res float64) {
+
+    const SidesTriangle myType = 3
+    const SidesSquare myType = 4
+    const SidesCircle myType = 0
+    if SidesTriangle == sidesNum {
+        res = (math.Pow(sideLen, 2.0) * math.Sqrt(3.0))/4
+    } else if SidesSquare == sidesNum {
+        res = math.Pow(sideLen, 2.0)
+    } else if SidesCircle == sidesNum {
+        res = math.Pi * math.Pow(sideLen, 2.0)
+    }
+    return
+}
