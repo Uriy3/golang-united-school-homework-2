abac820de13d73306cf233ab8613356a361b5406
Uriy3 <boginskiy291082@gmail.com>
2022-02-20 19:50:37 +0300

    My best solution

--- solution.go
+++ solution.go
@@ -1,32 +1,25 @@
 package square
+	
+import (
+	"math"
+)
 
-import "math"
+type Sides int
 
-type CustomType = int
+const (
+	SidesCircle   Sides = 0
+	SidesTriangle Sides = 3
+	SidesSquare   Sides = 4
+)
 
-const Pi = 3.14159
-const SidesCircle = 0
-const SidesTriangle = 3
-const SidesSquare = 4
-
-// Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
-
-// Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
-// it's like:
-// CalcSquare(10.0, SidesTriangle)
-// CalcSquare(10.0, SidesSquare)
-// CalcSquare(10.0, SidesCircle)
-//sideLen float64, sidesNum CustomType
-
-func CalcSquare(sideLen float64, sidesNum CustomType) float64 {
-	switch sidesNum {
-	case SidesSquare:
-		return sideLen * sideLen
-	case SidesTriangle:
-		return (math.Sqrt(3) / 4) * sideLen * sideLen
-	case SidesCircle:
-		return Pi * sideLen * sideLen
-	default:
+func CalcSquare(sideLen float64, sidesNum Sides) float64 {
+	if sidesNum == SidesCircle {
+		return math.Pi * math.Pow(sideLen, 2)
+	} else if sidesNum == SidesTriangle {
+		return (math.Sqrt(3) * math.Pow(sideLen, 2)) / 4
+	} else if sidesNum == SidesSquare {
+		return math.Pow(sideLen, 2)
+	} else {
 		return 0
 	}
 }
